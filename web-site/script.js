a28ca6a4cbe4c7a6842997df338700397b532aa3
Farid Hashimzada <hashimzadafarid@gmail.com>
2023-03-30 00:14:08 -0700

    home slider

--- web-site/script.js
+++ web-site/script.js
@@ -1,7 +1,23 @@
+$('.home-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
+
 let dropdown = document.querySelector('#dropdown')
 let mainDropdown = document.querySelector('#mainDropdown')
 let dropDown = document.querySelector('#dropDown')
 
+
+//introduction page dropdown
 dropdown.onclick = () => {
     mainDropdown.classList.toggle("active")
     // if(mainDropdown.style.display == "none"){
@@ -14,3 +30,6 @@ dropdown.onclick = () => {
 dropDown.onclick = () => {
     mainDropdown.classList.toggle('active')
 }
+
+//Ti Academy home page first carsel
+
