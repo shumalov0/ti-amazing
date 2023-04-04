7b9af1eace9c34413ca8b989e8e3f2017df76f68
shumalov0 <111644866+shumalov0@users.noreply.github.com>
2023-04-04 12:52:01 +0400

    complete studio pages

--- Js/script.js
+++ Js/script.js
@@ -3,3 +3,17 @@ function openLang(){
 
  lang.classList.toggle('active')
 }
+var swiper = new Swiper(".mySwiper", {
+  direction: "vertical",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+  },
+});
+
+    let hoverText = document.getElementByClassName('hover-text');
+    let img = document.getElementById('img');
+
