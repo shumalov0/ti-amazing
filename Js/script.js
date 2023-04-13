0e02786c37dbe007985745fdb7bf7733288ada76
shumalov0 <111644866+shumalov0@users.noreply.github.com>
2023-04-13 11:01:54 +0400

    complete responsive

--- Js/script.js
+++ Js/script.js
@@ -1,19 +1,15 @@
-function openLang(){
- let lang = document.getElementById('lang');
 
- lang.classList.toggle('active')
-}
-var swiper = new Swiper(".mySwiper", {
-  direction: "vertical",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  autoplay: {
-    delay: 3000,
-  },
-});
 
-    let hoverText = document.getElementByClassName('hover-text');
-    let img = document.getElementById('img');
+
+let burgerMenu = document.querySelector('.burger-menu');
+let NavBar = document.querySelector('.navBar');
+
+burgerMenu.addEventListener('click',()=>{
+   burgerMenu.classList.toggle('active');
+   NavBar.classList.toggle('active');
+    document.body.classList.toggle('stop-scrolling')
+})
+
+
+
 
